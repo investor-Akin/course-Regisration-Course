1aa3ef531ef8909b3fb447b99d71d8b8f1a906f3
investor-Akin <onigbindeakinbowale@gmail.com>
2025-07-09 00:28:22 +0100

    Enhance course registration form: add required fields and improve data handling in script

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
+let details = []
 function upload(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
 
+let name = studentName.value;
+let dept = department.value;
+let levelUser = level.value;
+let semesterUser = semester.value;
+let selectedRadio = outstandingInput.value;
+let selectedOutstanding = outstandingInput.value;
+
+// this checkboxes no too really clear i use Ai to make it clear and to write the code i no need to lie 
+let allCourseCheckboxes = document.querySelectorAll('.course-checkbox');
+let selectedCourses = [];
+for (let checkbox of allCourseCheckboxes) {
+    if (checkbox.checked) {
+        selectedCourses.push(checkbox.value);
+    }
+}
+
+let studentData = {
+    name: name,
+    dept: dept,
+    level: levelUser,
+    semester: semesterUser,
+    // hasOutstanding: selectedRadio.value,
+    hasOutstanding: selectedOutstanding, // Assuming this is a string "Yes" or "No"
+    courses: selectedCourses,
+};
+    // console.log(details);
+    // for (let index = 0; index < product.length; index++) {
+    //     // const element = array[index];
+    //     show.innerHTML += `
+    //     <tr>
+    //     <td>${product[index].name}</td>
+    //     <td>${product[index].price}</td>
+    //     <td>${product[index].quantity}</td>
+    //     <td>${product[index].description}</td>
+    //     </tr>`;
+    //     console.log("hello world!!!!!!!!!!!!!");
+        
+    // }
+
+    details.push(studentData);
+    for (let index = 0; index < details.length; index++) {
+        // const element = array[index];
+        show.innerHTML += `
+        <tr>
+            <td>${details[index].name}</td>
+            <td>${details[index].dept}</td>
+            <td>${details[index].level}</td>
+            <td>${details[index].semester}</td>
+            <td>${details[index].hasOutstanding}</td>
+            <td>${details[index].courses}</td>`
+    }
+
+    details = []
+    // Clear the form after submission also used Ai buh i understand the code
+    event.target.reset(); 
 }
